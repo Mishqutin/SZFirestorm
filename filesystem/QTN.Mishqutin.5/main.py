0244e5862d36a26b1c0645d633c7991dcd6fa325
Mishqutin <mundzioch@wp.pl>
2017-06-29 22:13:22 +0200

    Added backpack to store item types

---
+++ filesystem/QTN.Mishqutin.5/main.py
@@ -0,0 +1,28 @@
+import socket
+import os
+import time
+
+myName = "QTN.relayStation@localhost"
+
+f = open("dirInfo.szi", "r")
+dirInfo = eval(f.read())
+f.close()
+
+while True:
+    time.sleep(0.8)
+    f = open(dirInfo["mainDir"]+"\\cmd.txt", "r")
+    cmd = f.read()
+    f.close
+    if cmd==" ": continue
+    text = " ".join(eval(cmd))
+    f = open(dirInfo["mainDir"]+"\\cmd.txt", "w")
+    f.write(" ")
+    f.close()
+    
+    toSend = myName+"::"+text
+    
+    s = socket.socket()
+    s.connect(("localhost", 8192))
+    s.send(toSend.encode())
+    print(str(s.recv(4096)))
+    s.close()
